757ffd6c9e3371bfa206ec7c8d8e03975ab70175
aweliego <amelie.gorry.am@gmail.com>
2022-01-30 11:33:54 +0100

    Upload Part 2 of project

--- src/components/Business/Business.js
+++ src/components/Business/Business.js
@@ -1,40 +1,28 @@
 import React from 'react';
 import './Business.css';
 
-// The purpose of the <Business /> component is to represent how a business (a restaurant) in Ravenous will be formatted and styled. 
-
-const business = {
-  imageSrc: 'https://content.codecademy.com/programs/react/ravenous/pizza.jpg',
-  name: 'MarginOtto Pizzeria',
-  address: '1010 Paddington Way',
-  city: 'Flavortown',
-  state: 'NY',
-  zipCode: '10101',
-  category: 'Italian',
-  rating: 4.5,
-  reviewCount: 90,
-};
+// The purpose of the <Business /> component is to represent how a business (a restaurant) in Ravenous will be formatted and styled.
 
 class Business extends React.Component {
   render() {
     return (
       <div className="Business">
         <div className="image-container">
-          <img src={business.imageSrc} alt="" />
+          <img src={this.props.business.imageSrc} alt="" />
         </div>
-        <h2>{business.name}</h2>
+        <h2>{this.props.business.name}</h2>
         <div className="Business-information">
           <div className="Business-adress">
-            <p>{business.address}</p>
-            <p>{business.city}</p>
+            <p>{this.props.business.address}</p>
+            <p>{this.props.business.city}</p>
             <p>
-              {business.state} {business.zipCode}
+              {this.props.business.state} {this.props.business.zipCode}
             </p>
           </div>
           <div className="Business-reviews">
-            <h3>{business.category}</h3>
-            <h3 className="rating">{business.rating}</h3>
-            <p>{business.reviewCount}</p>
+            <h3>{this.props.business.category}</h3>
+            <h3 className="rating">{this.props.business.rating}</h3>
+            <p>{this.props.business.reviewCount}</p>
           </div>
         </div>
       </div>
